11cbdf4a095804712ef7c8abb9e12101728939c4
Oliver Blaser <kontakt@oblaser.ch>
2024-01-27 22:32:28 +0100

    added install script

---
+++ build/install-to-opt.sh
@@ -0,0 +1,9 @@
+
+# author        Oliver Blaser
+# date          27.01.2024
+# copyright     GNU GPLv3 - Copyright (c) 2024 Oliver Blaser
+
+
+sudo mkdir -p /opt/m3u-tool
+
+sudo cp cmake/m3u-tool /opt/m3u-tool/
